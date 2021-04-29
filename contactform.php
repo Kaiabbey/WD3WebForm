e98b69607bc39702c08f60e97876f09bf42fdce8
Kai.abbey <Kai.j.abbey@gmail.com>
2021-04-29 19:33:47 +1000

    added php

---
+++ contactform.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_POST["submit"]) ){
+    $name = $_POST['firstname']." ".$_POST['lastname'];
+    $emailfrom = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    $subject = $_POST['subject'];
+
+    $mailTo = "contactForm@kaiabbey.work";
+    $headers = "From: " . $emailfrom;
+    $emailMessage = "You have received an email from ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $emailMessage,$headers);
+    header("Location: index.php?mailsend");
+}
